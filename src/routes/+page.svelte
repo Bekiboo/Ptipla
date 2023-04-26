2aa4c5e9a49e4f1e362329e2429cb4f7e2913f54
Bekiboo <julien.connault@gmail.com>
2023-04-26 20:53:07 +0300

    Reset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let startDate: number = 2023;
+	let title = 'Mes Petits Plats';
+
+	const addOne = () => {
+		total += 1;
+	};
+
+	$: double = total * 2;
+
+	let total = 0;
+</script>
+
+<h1 class="pl-5 my-8">{title.toUpperCase()}</h1>
+<p>De mère en filles depuis {startDate}</p>
+
+<button on:click={addOne}>Add 1</button>
+<div>Total: {total} carotte{total > 1 ? '' : 's'}</div>
+<div>Double: {double}</div>
+
+<style>
+</style>
