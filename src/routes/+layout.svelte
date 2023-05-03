42e4e628000c5bf206be1027c45c8d78021f88dd
Bekiboo <julien.connault@gmail.com>
2023-05-03 13:44:30 +0300

    Fix Auth

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,29 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import Navbar from './Navbar.svelte';
 	import SideNav from './SideNav.svelte';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
+
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
 </script>
+
+<svelte:head>
+	<title>User Management</title>
+</svelte:head>
 
 <Navbar />
 
--- src/routes/SideNav.svelte
+++ src/routes/SideNav.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
-    
 	// catalogue recettes --> filters/search
 	// créer nouvelle recette
 	// favorites
 </script>
 
-<aside class="bg-green-400 w-48 h-full">
-    <div class="image"></div>
+<aside class="bg-green-400 w-48 h-screen">
+	<div class="image" />
 
-    <ul>
-        <li><a href="/recette">Mes Recettes</a></li>
-        <li><a href="">Créer nouveau</a></li>
-        <li><a href="">Favorites</a></li>
-    </ul>
+	<ul>
+		<li><a href="/recette">Mes Recettes</a></li>
+		<li><a href="">Créer nouveau</a></li>
+		<li><a href="">Favorites</a></li>
+	</ul>
 </aside>
-
